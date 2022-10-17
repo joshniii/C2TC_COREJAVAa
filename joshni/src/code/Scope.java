5c9b0b0767939190917278c026383443fe28bfd8
admin <admin@DESKTOP-VPMPMI8>
2022-10-17 17:38:39 +0530

    first commit

---
+++ joshni/src/code/Scope.java
@@ -0,0 +1,17 @@
+package code;
+
+public class Scope {
+
+	public static void main(String[] args) {
+		int outer=3;
+		{
+			int inner=40;
+			System.out.println(inner);
+			System.out.println(outer);
+		}
+		int inner=3;
+		System.out.println(inner);
+
+	}
+
+}
