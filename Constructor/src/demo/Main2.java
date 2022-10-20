809fb9b24ee6616b05422c503635309ef72a80df
admin <admin@DESKTOP-VPMPMI8>
2022-10-20 21:48:28 +0530

    first commit

--- Constructor/src/demo/Main2.java
+++ Constructor/src/demo/Main2.java
@@ -1,27 +1,27 @@
 package demo;
-class Employee {
+class Employee1 {
 	int id;
 	String name;
 	float salary;
-	Employee() {
+	Employee1(int id, String name, float salary) { 
 		System.out.println(" User defined no-argument"  + "constructor executed");
 	}
 	void display() {
 		System.out.println(id + " " + name + " " + salary);
 	}
 }
-public class Main1 {
+public class Main2 {
 
 	public static void main(String[] args) {
 		
-		Employee e1= new Employee();
-		Employee e2= new Employee();
+		Employee1 e1= new Employee1(101,"joshni", 45000);
+		Employee1 e2= new Employee1(102,"jyothi",25000);
+		Employee1 e3= new Employee1(103,"vihaan",55000);
 		
 		e1.display();
 		e2.display();
-		
+		e3.display();
 	}
 
 }
-
 
